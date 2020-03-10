65a8f704eeaf1000aa8d7f9c58817531370f2ddf
Zanhua1999 <skyair886@gmail.com>
2020-03-10 10:38:26 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,8 @@
+var informatie = {
+"Naam: ":"Zara",
+"Score: ": 36,
+"Begin datum: ":"21 Januarie 2016",
+"Vrienden: " : ["Mika", "Tom", "Maria", "Sumail", "Lotte"],
+"Gekwalificeerd: " : true,
+}
+console.log(informatie);
